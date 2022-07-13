9390519991f0b55c6abcad5965f764820de7a57c
mhkalan <89645282+mhkalan@users.noreply.github.com>
2022-07-13 19:06:17 +0430

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import TodoList from "./components/TodoList";
+function App() {
+  return (
+    <div className='todo-app'>
+      <h2>To Do App</h2>
+      <TodoList/>
+    </div>
+  );
+}
+
+export default App;
